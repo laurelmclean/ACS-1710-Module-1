86a4955d8bdda219196609cb475fa947ac672cdc
Laurel McLean <laurel.mclean@live.ca>
2022-10-25 13:12:59 -0600

    Completed favorite animal

--- app.py
+++ app.py
@@ -12,6 +12,16 @@ def homepage():
     # returns the page contents that will show up in browser
     return 'Are you there, world? It\'s me Ducky!'
 
+
+# route for user to add their favourite animal
+# URL will be dictated by what animal they select
+@app.route('/animal/<users_animal>')
+def favorite_animal(users_animal):
+    # display message based on user input
+    return f'Wow, {users_animal} is my favorite animal, too!'
+
+
 # tell python how to run the server
+# Always needs to be at the very bottom
 if __name__ == '__main__':
     app.run(debug=True)
