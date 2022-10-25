922fdd56dfb3b8a839a736b7df255920a77f1ad5
Laurel McLean <laurel.mclean@live.ca>
2022-10-25 12:51:42 -0600

    Completed hello world tutorial

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
+# import Flask library
 
-# TODO: Follow the assignment instructions to complete the required routes!
-# (And make sure to delete this TODO message when you're done!)
+from flask import Flask
+
+# set up app variable to enable us to write routes
+
+app= Flask(__name__)
+
+# indicates URL of webpage which is homepage
+@app.route('/')
+def homepage():
+    # returns the page contents that will show up in browser
+    return 'Are you there, world? It\'s me Ducky!'
+
+# tell python how to run the server
+if __name__ == '__main__':
+    app.run(debug=True)
