017c7015206344979b62cfb15a0b71f4964efa3c
Laurel McLean <laurel.mclean@live.ca>
2022-10-27 11:19:37 -0600

    Completed dice roll

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 # import Flask library
 
+from ast import For
 from flask import Flask
+
+# import random
+
+import random
 
 # set up app variable to enable us to write routes
 
@@ -49,13 +54,23 @@ def multiply(number1, number2):
 
 @app.route('/sayntimes/<word>/<n>')
 def say_n_times(word, n):
-    # validates and loops
-    if number1.isdigit() and number2.isdigit() == True:
-        total = int(number1) * int(number2)
-        return f'{number1} times {number2} is {total}!'
+    # validates and prints
+    if n.isdigit() == True:
+        string = (word + ' ')
+        return f'{string * int(n)}'
     else:
-        return "Invalid inputs. Please try again by entering 2 numbers!"
+        return "Invalid input. Please try again by entering a word and a number!"
 
+# dice game
+
+@app.route('/dicegame')
+def dice_game():
+    roll = random.randint(1, 6)
+    # validates and prints
+    if roll < 6:
+        return f'You rolled a {roll}. You lost!'
+    else:
+        return f'You rolled a {roll}. You won!'
 
 # tell python how to run the server
 # Always needs to be at the very bottom
